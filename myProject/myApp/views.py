28c2ab38c2cef422d0d5a070cff5d94293e4b42e
TwoTanawin <tanawin.twox@gmail.com>
2023-12-27 18:11:32 +0700

    modify database

--- myProject/myApp/views.py
+++ myProject/myApp/views.py
@@ -1,17 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from myApp.models import Person
 
 # Create your views here.
 def index(request):
-    name = 'admin'
-    age = 23
+    # name = 'admin'
+    # age = 23
 
-    return render(request, 
-                  "index.html", 
-                  {"name":name, "age":age})
+    # return render(request, 
+    #               "index.html", 
+    #               {"name":name, "age":age})
+    allPerson = Person.objects.all()
+
+    # allPerson = Person.objects.filter(age=24)
+    return render(request, "index.html", {"allPerson":allPerson})
 
 def about(request):
     return render(request, "about.html")
 
 def form(request):
-    return render(request, "form.html")
+    if request.method == "POST":
+        #recieve
+        name = request.POST["name"]
+        age = request.POST["age"]
+
+        #save
+        person = Person.objects.create(
+            name = name,
+            age=age
+        )
+
+        person.save()
+        # print(name, age)
+
+        # change direction
+        return redirect("/")
+    else:
+        return render(request, "form.html")
