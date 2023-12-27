65f9bd82edd4548c54f99e063c4256e386f54837
TwoTanawin <tanawin.twox@gmail.com>
2023-12-27 18:48:17 +0700

    complete

--- myProject/myApp/urls.py
+++ myProject/myApp/urls.py
@@ -4,5 +4,7 @@ from myApp import views
 urlpatterns = [
     path('',views.index), # '' mean first pages
     path('about',views.about),
-    path('form',views.form)
+    path('form',views.form),
+    path('edit/<personID>',views.edit),
+    path('delete/<personID>',views.delete)
 ]
--- myProject/myApp/views.py
+++ myProject/myApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from myApp.models import Person
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -32,8 +33,35 @@ def form(request):
 
         person.save()
         # print(name, age)
+        messages.success(request, "Submit Success")
 
         # change direction
         return redirect("/")
     else:
         return render(request, "form.html")
+    
+def edit(request, personID):
+    if request.method == "POST":
+        person = Person.objects.get(id=personID) # check person id
+        person.name = request.POST["name"]
+        person.age = request.POST["age"]
+        person.save()
+
+        # change direction
+        return redirect("/")
+
+    else:
+        # get data
+        person = Person.objects.get(id=personID)
+
+
+        return render(request, "edit.html",{"person":person})
+    
+def delete(request, personID):
+    person = Person.objects.get(id=personID)
+    person.delete()
+
+    messages.success(request, "Deleted Success")
+
+    # change direction
+    return redirect("/")
